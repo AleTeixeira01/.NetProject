9224d1bd8d0f4034b69bbc5566c5d75e91c90b98
AleTeixeira01 <alexandre.t01@outlook.com>
2020-07-17 08:04:38 -0300

    Validation

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -9,7 +9,7 @@ namespace SalesWebMvc.Models
     {
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "{0} required")]
+        [Required(ErrorMessage = "{0} required")]// {0} equals Name
         [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1}")]
         public string Name { get; set; }
 
